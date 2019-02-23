8a49d761941c124159b9d18e22e5aea3e26a43a1
sureshhs <47510939+sureshhs@users.noreply.github.com>
2019-02-22 18:07:31 -0800

    add junit tests

---
+++ src/main/java/Com/Shival/JavaJUnit/App.java
@@ -0,0 +1,13 @@
+package Com.Shival.JavaJUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Com/Shival/JavaJUnit/MyUnitTests.java
@@ -0,0 +1,22 @@
+package Com.Shival.JavaJUnit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MyUnitTests {
+	
+		@Test
+		public void JUnitTest1() {
+			assertEquals(100, 100);
+		}
+		
+		@Test
+		public void JUnitTest2() {
+			assertEquals(100, 100);
+		}	
+		
+		@Test
+		public void JUnitTest3() {
+			assertEquals(100, 200);
+		}		
+}
